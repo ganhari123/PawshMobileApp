09afa7622bcce1785dd94a3d6023dd87c6d8bddd
Arunima <arunimaprakash@gmail.com>
2019-09-24 02:17:37 -0400

    Signup with Checkbox and PhoneInput :in progress

--- app/views/SignUp.js
+++ app/views/SignUp.js
@@ -10,6 +10,8 @@ import {
 
 import { GroomerButton } from "../Components/Buttons/GroomerButton"
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import CheckBox from 'react-native-check-box'
+import PhoneInput from 'react-native-phone-input'
 
 const { height, width } = Dimensions.get('window');
 
@@ -22,14 +24,14 @@ export class SignUp extends React.Component {
                 resetScrollToCoords={{ x: 0, y: 0 }}
                 scrollEnabled={true}
             >
-                <View style={{flex: 0, marginLeft: 0.1 * width, marginTop: 0.07 * height, marginBottom: 0.05 * height, height: 70}}>
+                <View style={{flex: 0, marginLeft: 0.1 * width, marginTop: 0.05 * height, marginBottom: 0.05 * height, height: 70}}>
                     <Text style={{paddingBottom: 5, fontSize: 36, fontWeight: 'bold'}}>Hello!</Text>
                     <Text style={{fontSize: 16, color: '#A0A0A0'}}>We're glad you want to join us!</Text>
                 </View>
 
                 <View style={{flex: 0, marginLeft: 0.1 * width, height: 70, marginBottom: 0.05 * height}}>
                     <Text style={{fontSize: 16, marginBottom: 15, fontWeight: 'bold'}}>Name</Text>
-                    <TextInput style={{width: 0.8 * width, borderBottomColor: '#A0A0A0', borderBottomWidth: 1}}placeholder='Puppy Parent' secureTextEntry={true}/>
+                    <TextInput style={{width: 0.8 * width, borderBottomColor: '#A0A0A0', borderBottomWidth: 1}}placeholder='Puppy Parent'/>
                 </View>
 
             
@@ -45,8 +47,13 @@ export class SignUp extends React.Component {
 
                 <View style={{flex: 0, marginLeft: 0.1 * width, height: 70, marginBottom: 0.05 * height}}>
                     <Text style={{fontSize: 16, marginBottom: 15, fontWeight: 'bold'}}>Phone Number</Text>
-                    <TextInput style={{width: 0.8 * width, borderBottomColor: '#A0A0A0', borderBottomWidth: 1}}placeholder='00 000 000 0000' secureTextEntry={true}/>
+                    <PhoneInput ref='phone' style={{width: 0.8 * width, borderBottomColor: '#A0A0A0', borderBottomWidth: 1}}/>
                 </View>
+
+                <View style={{flex: 0, flexDirection:'row' , marginLeft: 0.3 * width, height: 50, marginBottom: 0.0005 * height}}>
+                    <CheckBox />
+                    <Text style={{fontSize: 12, marginBottom: 15, color: '#A0A0A0'}}>I agree to<Text style={{fontSize: 12, marginBottom: 15, color: '#3BC8CA'}} onPress={() => this.props.navigation.navigate('TermsRT')}>Terms and Conditions</Text></Text>
+                    </View>
 
 
                 <GroomerButton buttonText="Sign Up"/>
