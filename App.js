c0b3f99fc2324391f1ad923974c2e8b8d307de65
Arunima <arunimaprakash@gmail.com>
2019-09-23 23:40:02 -0400

    Signup Screen Completed

--- App.js
+++ App.js
@@ -8,13 +8,15 @@
 
 import React, {Fragment} from 'react';
 import { HomePage } from './app/views/HomePage.js';
-import { SignIn } from './app/views/SignIn.js'
+import { SignIn } from './app/views/SignIn.js';
+import { SignUp } from './app/views/SignUp.js';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
 const MainNavigator = createStackNavigator({
   HomeRT: {screen: HomePage},
   LoginRT: {screen: SignIn},
+  SignupRT: {screen: SignUp},
 }, {
   initialRouteName: 'HomeRT',
   headerMode: 'none',
